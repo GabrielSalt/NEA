4d14a88e240c7218a73fdde551b871aa73867c42
GabrielSalt <salt.gabriel@gmail.com>
2022-10-30 20:18:58 +0000

    Update CameraScreen.js

--- components/CameraScreen.js
+++ components/CameraScreen.js
@@ -1,61 +1,57 @@
-import Icon from 'react-native-vector-icons/Feather';
-import React, { useState, useEffect, useRef, Component} from 'react'
-import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity } from 'react-native';
-import { AutoFocus, Camera, CameraType } from 'expo-camera';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet ,Text, View, Button, Image} from 'react-native';
+import { Camera } from 'expo-camera';
 import { useIsFocused } from '@react-navigation/native';
 
-export default function CameraScreen( {route, navigation}) {
-    const isFocused = useIsFocused();
-    const [Permission, setPermission] = useState(Camera.useCameraPermissions())
-    const [cameraRef, setCameraRef] = useState(null)
-    const [enabled, setEnabled] = useState(false)
+export default function CameraScreen( { route, navigation } ) {
+  const isFocused = useIsFocused()
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
 
-    useEffect(() => {
-        (async() => {
-        const { status } = await Camera.requestCameraPermissionsAsync()
-        setPermission(status === 'granted')
-        })
-    })
-
-    async function takePhoto(){
-        setEnabled(false)
-        const data = await cameraRef.takePictureAsync({base64: true})
-        navigation.navigate('Confirm', {image: data.uri})
-    }
-
-    function onCameraReady () {
-        setEnabled(true)
-    }
-
-    if (Permission === null) {
-        return(
-            <Text> Camera Unavailable, Please Use Manual Setter </Text>
-        )
-    }
-    else if (isFocused) {
-        return(
-                <Camera ref={ref => {setCameraRef(ref)}} type={CameraType.back} ratio='1:1' style={styles.Camera} onCameraReady={onCameraReady}>
-                    <View style={styles.ButtonContainer}>
-                        <TouchableOpacity onPress={takePhoto} style={styles.Button} disabled={!enabled}>
-                            <Icon size={36} color='#FFFFFF' name='circle'></Icon>
-                        </TouchableOpacity>
-                    </View>
-                </Camera>
-        )
-    }
-    else {
-        return <Text>Not focused</Text>
-    }
+useEffect(() => {
+    (async () => {
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === 'granted');
+})();
+  }, []);
+const takePicture = async () => {
+    if(camera){
+        const data = await camera.takePictureAsync(null)
+        setImage(data.uri)
+  }
 }
 
+  if (hasCameraPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  else if (isFocused){
+  return (
+   <View style={{ flex: 1}}>
+    <View style={styles.cameraContainer}>
+            <Camera 
+            ref={ref => setCamera(ref)}
+            style={styles.fixedRatio} 
+            type={Camera.Constants.Type.back}
+            ratio={'1:1'} />
+      </View>
+       <Button title="Take Picture" onPress={() => takePicture()} />
+        {image && <Image source={{uri: image}} style={{flex:1}}/>}
+        {image && <Button title='Confirm' onPress={() => navigation.navigate('Confirm', {image})}/>}
+   </View>
+  );
+  }
+  else{
+    return <View></View>
+  }
+}
 const styles = StyleSheet.create({
-    Camera: {
-        marginTop: '30%',
-        width: '100%',
-        aspectRatio: 1,
-    },
-    ButtonContainer: {
-        marginTop: '90%',
-        alignSelf: 'center',
-    },
-});
+  cameraContainer: {
+      flex: 1,
+      flexDirection: 'row'
+  },
+  fixedRatio:{
+      flex: 1,
+      aspectRatio: 1
+  }
+})
